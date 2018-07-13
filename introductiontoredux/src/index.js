1fe21b13b6e6bbeb19b4554e766b43a28b6d9fd2
Ryan McAfee <admin@ryanmcafee.com>
2018-07-13 02:09:56 -0500

    Revisements & additions to slides

--- introductiontoredux/src/index.js
+++ introductiontoredux/src/index.js
@@ -1,4 +1,11 @@
 import "./styles.css";
 import "./template.html";
 
-var slideshow = remark.create({ ratio: "16:9" });
+var slideshow = remark.create({ 
+    ratio: "16:9", 
+    navigation: {
+        scroll: true,
+        touch: true,
+        click: false
+    } 
+});
